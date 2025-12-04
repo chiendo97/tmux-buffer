ad74faee5e34a1f93633a963ed08b8cc4838cc33
chiendo97 <chiendo97@gmail.com>
2025-12-04 10:23:39 +0700

    update

--- buffex.sh
+++ buffex.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 tmp_file=$(mktemp) 
-tmux capture-pane -J -S -3000
-tmux save-buffer "$tmp_file"
+tmux capture-pane -pJS -3000 > "$tmp_file"
 tmux display-popup -E -w 90% -h 90% "nvim '-c set ft=bash' + $tmp_file"
