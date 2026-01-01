7a390f8320213f22f62d4355d07f19c21dd9e632
chiendo97 <chiendo97@gmail.com>
2026-01-01 20:57:46 +0700

    feat: open popup same dir

--- buffex.sh
+++ buffex.sh
@@ -2,4 +2,4 @@
 
 tmp_file=$(mktemp) 
 tmux capture-pane -pJS -3000 > "$tmp_file"
-tmux display-popup -E -w 90% -h 90% "nvim '-c set ft=bash' + $tmp_file"
+tmux display-popup -d '#{pane_current_path}' -E -w 90% -h 90% "nvim '-c set ft=bash' + $tmp_file"
